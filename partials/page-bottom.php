b14807d617f75299f63ff50601815caf89d538ac
Estevão Rolim <estevao.prolim@gmail.com>
2021-05-26 20:06:34 +0100

    Bottom color change

--- partials/page-bottom.php
+++ partials/page-bottom.php
@@ -12,6 +12,6 @@ $text_aos = 'data-aos="fade" data-aos-delay="200"' . $common_aos;
 <div class="page-bottom d-flex p-1">
     <small class="m-auto text-center text" <?php echo $text_aos; ?>>
         2021 © <strong>Emertech Project</strong>. <br class="d-block d-md-none">
-        Desenvolvido por <a href="https://www.linkedin.com/in/estevaoprolim/">Estevão Rolim</a>
+        Desenvolvido por <a href="https://www.linkedin.com/in/estevaoprolim/">Estevão Rolim</a>.
     </small>
 </div>
