7b9e9ab1b904b11571a9b08c60a8369ef797313e
Estevão Rolim <estevao.prolim@gmail.com>
2021-08-12 16:53:03 +0100

    Remove video overlay support from header

--- partials/page/header.php
+++ partials/page/header.php
@@ -27,33 +27,15 @@ if($show_video):
 <header class="navbar navbar-expand-lg pe-3 pe-lg-4 ps-0 py-3 text-light" id="header">
 	<div class="container-fluid p-0">
 		<div class="navbar-brand me-auto p-0">
-			<!--
-			<?php
-				if($show_video == 1) {
-					?>
-						<a href="<?php echo home_url(); ?>">
-							<video autoplay muted width="300">
-								<source src="<?php echo $video_src; ?>" type="">
-								<?php 
-									// the_custom_logo();
-								?>
-							</video> 
-						</a>
-					<?php
-				}
-				else {
-					the_custom_logo(); 
-				}
-			?> 
-			-->
 			
 			<?php 
 				the_custom_logo(); 
 			?>
 		</div>
 
-		<button class="navbar-toggler p-0" type="button" data-bs-toggle="collapse" data-bs-target="#mainMenuDropdown" aria-controls="mainMenuDropdown" aria-expanded="false" aria-label="<?php echo __("Ativar Menu", "emertech") ?>">
-			<!-- <span class="navbar-toggler-icon"></span> -->
+		<button class="navbar-toggler p-0" type="button" 
+		data-bs-toggle="collapse" data-bs-target="#mainMenuDropdown" 
+		aria-controls="mainMenuDropdown" aria-expanded="false" aria-label="<?php echo __("Ativar Menu", "emertech") ?>">
 			<span class="icon bi bi-list"></span>
 		</button>
 
